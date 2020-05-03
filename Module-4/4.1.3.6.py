7d8b6acf7896ef659af84c94624d7b2477b0d48d
nonkronk <nonkronkblues@gmail.com>
2020-05-03 14:15:07 +0700

    add Module-4

---
+++ Module-4/4.1.3.6.py
@@ -0,0 +1,17 @@
+def isYearLeap(year):
+    if year % 400 == 0 or (year % 4 == 0 and year % 100 != 0):
+        return True
+    else:
+        return False
+
+testData = [1900, 2000, 2016, 1987]
+testResults = [False, True, True, False]
+
+for i in range(len(testData)):
+    yr = testData[i]
+    print(yr, "->", end="")
+    result = isYearLeap(yr)
+    if result == testResults[i]:
+        print("OK")
+    else:
+        print("Failed")
