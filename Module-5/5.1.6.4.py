302a7188fcf42f7d7bb76ff5040095b9fe9dc7fe
nonkronkblues@gmail.com <nonkronkblues@gmail.com>
2020-06-22 02:29:59 +0700

    submitted quiz

--- Module-5/5.1.6.4.py
+++ Module-5/5.1.6.4.py
@@ -1,5 +1,16 @@
-def readint(prompt, minim, maxim):
-    pass
+def readint(prompt, min, max):
+    while True:
+        val = input(prompt)
+        try:
+            val = int(val)
+            if val >=  min and val <= max:
+                break
+            else:
+                print(f"Error: the number is not within permitted range ({min}, {max})")        
+        except:
+            print("Error: wrong input")
+    return val
 
 v = readint("Enter a number from -10 to 10: ", -10, 10)
-print("The number is: ", v)
+
+print("The number is:", v)
