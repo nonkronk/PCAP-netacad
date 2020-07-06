d74e842b3283f17f51efdced268b40b2ef99e906
nonkronkblues@gmail.com <nonkronkblues@gmail.com>
2020-07-06 20:37:46 +0700

    read txt file and count each alphabet char

---
+++ Module-6/6.1.9.15.py
@@ -0,0 +1,18 @@
+fn = input('Enter filename: ')
+
+try:
+    f = open(fn, 'rt', errors='ignore')
+    letter = {}
+    for i in range(ord('a'), ord('a') + 26):
+        letter[chr(i)] = 0
+    for line in f:
+        line.lower()
+        for ch in line:
+            if ch in letter.keys():
+                letter[ch] += 1
+    for k, v in letter.items():
+        if v != 0:
+            print(k, '->', v)
+
+except Exception as e:
+    print(e)
